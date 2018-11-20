ceac91f5ec9d8e176644d5f719aad5c8175a90ad
petermeissner <retep.meissner@gmail.com>
2018-11-20 18:02:13 +0100

    wpd_get_exact_worker()

--- R/wpd_get_exact_worker.R
+++ R/wpd_get_exact_worker.R
@@ -1,3 +1,7 @@
+#' wpd_get_exact_worker
+#'
+#' @rdname wpd_get_exact
+#'
 wpd_get_exact_worker <-
   function(page, lang){
     # generate file path
@@ -11,10 +15,20 @@ wpd_get_exact_worker <-
     con <-
       DBI::dbConnect(
         RSQLite::SQLite(),
-        dbname = file_path
+        dbname   = file_path,
+        flags    = RSQLite::SQLITE_RO,
+        encoding = "UTF-8"
       )
 
     # query database
-
+    DBI::dbGetQuery(
+      con,
+      sprintf(
+        "select * from dict_%s where page_name = '%s'",
+        lang,
+        page
+      )
+    )
 
   }
+
